38182c9f8b5d658403ea5a98533db60708543dac
pcouaillier <paul.couaillier@laposte.net>
2018-08-23 21:46:04 +0200

    remplacement de la mécanique de téléchargement

--- chan/chan.ts
+++ chan/chan.ts
@@ -13,55 +13,222 @@ cache.autoplay = false;
 
 namespace Downloader
 {
-  function extractExtension(str: string) {
-	  let splitStr = str.split('.');
-    splitStr.pop();
-    return splitStr.join('.');
-  }
+  let checkLocalStorage = true;
+  let optimizedForFourChan = document.URL.includes('4chan.org') || document.URL.includes('8ch.net');
 
-  function findName(elem: Element): string {
-    if (elem.children.length === 0 ) {
-      if (elem.getAttribute('title')) {
-        return extractExtension(elem.getAttribute('title').trim());
-      }
-      return extractExtension(elem.innerHTML.trim());
-    }
-    return findName(elem.children[0]);
-  }
+    'use strict';
+    const STORAGE_NAME = 'chan_database';
+    const dlAll = (() => {
+        const fourChanSelectors = {
+            commentBody: 'blockquote',
+            graphLink: '.backlink .quotelink',
+            topElemRegex: /^(pc|t)[0-9]{4}[0-9]+$/,
+            files: '.file .fileText a'
+        };
+        const heightChanSelectors = {
+            commentBody: '.body',
+            graphLink: '.mentioned  a',
+            topElemRegex: /^(reply_|thread_)[0-9]+$/,
+            files: '.file .fileinfo a'
+        };
+        let currentContextSelectors = document.URL.includes('4chan.org') ? fourChanSelectors
+                                                                         : heightChanSelectors;
 
-  document.getElementsByTagName('body')[0].appendChild((()=>{
-    let b = document.createElement('button');
-    b.style.position = "fixed";
-    b.style.top = "3rem";
-    b.style.right = "0.5rem";
-    b.innerHTML = "[ download ]";
-
-    b.addEventListener('click',()=> {
-      let s = '';
-      const load = (e: HTMLAnchorElement) => {
-        let l = e.href.split('.'),
-            ext = l[l.length - 1].toLowerCase(),
-            append = false;
-
-        if ((ext === 'jpg' || ext === 'webm' || ext === 'mp4' || ext === 'png' || ext === 'gif' || ext === 'rar' || ext === 'zip' || ext === '7z') || (!(ext !== '' && ext.length < 6 && ext !== 'html' && ext!=="html#" && ext !== 'php' && ext !== 'htm' && ext[ext.length - 1] !== '/') && confirm(ext))) {
-            append = true;
+        function commentContent(elem: Element) {
+            let a = elem.querySelector(currentContextSelectors.commentBody);
+            return a ? (a as HTMLElement).innerText : null;
         }
 
-        if (append)
-            s += 'wget -nc -O "'+ findName(e).trim() + "__" + (Math.random()*1000000 | 0).toString() + '.' + ext +'" ' + e.href + ' ; ';
-      };
-      let list = document.querySelectorAll('.file > .fileText > a');
-      if (list.length > 0) {
-        Array.prototype.forEach.call(list, load);
-      }
-      list = document.querySelectorAll('.hyperlinkMediaFileName');
-      if (list.length > 0) {
-        Array.prototype.forEach.call(list, load);
-      }
-      window.prompt('copy', s);
-    });
-    return b;
-  })());
+        function consumeHrefSelector(selector: string) {
+            let regResult = /^#([0-9]+)$/.exec(selector);
+            return regResult ? document.getElementById(regResult[1]) : document.querySelector(selector);
+        }
+
+        function commentGraph(elem: Element) {
+            return {
+                comment: commentContent(elem),
+                links: Array.prototype.map.call(
+                    elem.querySelectorAll(currentContextSelectors.graphLink),
+                    (a: HTMLElement) => commentGraph(getTop(consumeHrefSelector(a.getAttribute('href'))))
+                )
+            };
+        }
+
+        function getTop(a: Element): Element {
+            if ( a === document.body) throw {error: "topLevelAcchived"};
+
+            let id = a.getAttribute('id');
+            if (!id || currentContextSelectors.topElemRegex.exec(id)===null) {
+                return getTop(a.parentElement);
+            }
+            return a;
+        }
+
+        function generateCommentGraph(a: Element): {comment: string, links: any} {
+            return commentGraph(getTop(a));
+        }
+
+
+        function download(file: any) {
+            let a = document.createElement('a'),
+                url = URL.createObjectURL(file);
+            a.href = url;
+            a.setAttribute('download', file.name);
+            document.body.appendChild(a);
+            a.click();
+            URL.revokeObjectURL(url);
+            document.body.removeChild(a);
+        }
+
+        function removeUnwantedChars(str: string) {
+            if (!str) return '';
+            const remove_chars = ['"', '#', '%', '*', ':', '<', '>', '?', '/', '\\', '|', '\'', ',', ';'];
+            const escape_chars = ['!'];
+            str = str.trim();
+            let res = '';
+            for (let i = 0; i < str.length; i++) {
+                if ((escape_chars as any).includes(str[i]))
+                    res += '\\' + str[i];
+                else if (!(remove_chars as any).includes(str[i]))
+                    res += str[i];
+            }
+            return res.trim();
+        }
+
+        function getThreadNameFromTitle() {
+            let name = document.title.match(/^.* - (.*) - .* - .*$/);
+            return (name && name.length>0) ? name[1] : '';
+        }
+
+        return function dlAll() {
+            let threadName = getThreadNameFromTitle(),
+                tree = Array.prototype.map.call(
+                    document.querySelectorAll(currentContextSelectors.files),
+                    (a: HTMLAnchorElement) => ({
+                        name: a.title && a.title!=='' ? a.title : a.innerText,
+                        link: a.href,
+                        commentGraph: generateCommentGraph(a)
+                    })
+                );
+            console.log(threadName, getThreadNameFromTitle, getThreadNameFromTitle());
+
+            if (threadName===null || threadName.trim()==='') {
+                threadName = document.querySelector<HTMLElement>('span.subject').innerText;
+            }
+            if (threadName===null || threadName.trim()==='') {
+                threadName = document.querySelector<HTMLElement>('span.name').innerText;
+            }
+
+            let escapedThreadName = removeUnwantedChars(threadName),
+                dlFileHead = [
+                    '#!/bin/bash',
+                    'mkdir -p "'+threadName+'"',
+                    'cd "' + threadName +'"',
+                    'mv "${0/__dl\.sh/__comments\.json}" .',
+                    'mv "$0" .',
+                ].join('\n') + '\n';
+
+            download(
+                new File(
+                    [JSON.stringify({threadName: threadName, threadUrl: document.URL.toString(), tree: tree})],
+                    escapedThreadName + '__comments.json',
+                    {type: 'text/json'}
+                )
+            );
+            download(
+                new File(
+                    [
+                        tree.reduce(
+                            (acc: string, a: any) =>
+                                acc += 'wget -nc -O "' + removeUnwantedChars(a.name) + '" "' + a.link + '"\n',
+                            dlFileHead
+                        )
+                    ],
+                    escapedThreadName + '__dl.sh',
+                    {type: 'text/sh'}
+                )
+            );
+        };
+    })();
+    const loadFromCache = () => {
+        if (window.localStorage) {
+            var json = (window.localStorage.getItem(STORAGE_NAME));
+            return json ? JSON.parse(json) : [];
+        }
+        else {
+            return [];
+        }
+    };
+    function cleanName(str: string) {
+        const remove_chars = ['"', '#', '%', '*', ':', '<', '>', '?', '/', '\\', '|', '\'', ',', ';'];
+        const escape_chars = ['!'];
+        str = str.trim();
+        var res = '';
+        for (var i = 0; i < str.length; i++) {
+            if ((escape_chars as any).includes(str[i]))
+                res += '\\' + str[i];
+            else if (!(remove_chars as any).includes(str[i]))
+                res += str[i];
+        }
+        return res;
+    }
+    function extractExtension(str: string) {
+        var sstr = str.split('.');
+        sstr.pop();
+        return sstr.join('.');
+    }
+    function findName(elem: HTMLElement): string {
+        if (elem.children.length === 0)
+        {
+            if (elem.title && elem.title.length>0) {
+                return extractExtension(elem.title.trim());
+            }
+            return extractExtension(elem.innerHTML.trim());
+        }
+        return findName(elem.children[0] as HTMLElement);
+    }
+    document.getElementsByTagName('body')[0].appendChild((() => {
+        var b = document.createElement('button');
+        b.style.position = 'fixed';
+        b.style.top = '3rem';
+        b.style.right = '0.5rem';
+        b.innerHTML = '[ download ]';
+        b.addEventListener('click', function bashDL() {
+            if (optimizedForFourChan) {
+                dlAll();
+                return;
+            }
+            var alreadyDownloaded = loadFromCache();
+            var s = '';
+            const load = (e: HTMLAnchorElement) => {
+                var l = e.href.split('.');
+                var ext = l[l.length - 1].toLowerCase();
+                var append = false;
+                if ((ext === 'jpg' || ext === 'webm' || ext === 'mp4' || ext === 'png' || ext === 'gif' || ext === 'rar' || ext === 'zip' || ext === '7z') || (!(ext !== '' && ext.length < 6 && ext !== 'html' && ext !== 'html#' && ext !== 'php' && ext !== 'htm' && ext[ext.length - 1] !== '/') && confirm(ext)))
+                    append = true;
+                var name = cleanName(e.innerHTML);
+                if (name === '')
+                    name = e.href;
+                if (append && (!checkLocalStorage || !alreadyDownloaded.includes(e.href))) {
+                    if (!alreadyDownloaded.includes(e.href))
+                        alreadyDownloaded.push(e.href);
+
+                    s += 'wget -nc -O "' + cleanName(findName(e)) + "__" + ((Math.random() * 1000000) | 0).toString() + '.' + ext + '" ' + e.href + ' ; ';
+                }
+            };
+            [
+                document.querySelectorAll('.file > .fileText > a'),
+                document.querySelectorAll('.fileinfo > span > a'),
+                document.querySelectorAll('.hyperlinkMediaFileName')
+            ].forEach((list)=>{
+                if (list.length > 0) Array.prototype.forEach.call(list, load);
+            });
+            window.prompt('copy', s);
+            if (window.localStorage)
+                window.localStorage.setItem(STORAGE_NAME, JSON.stringify(alreadyDownloaded));
+        });
+        return b;
+    })());
 }
 
 namespace Player
